379e124032107e2de4480bf121d419f17b285ae0
Andres Andrade <aaandrades@outlook.com>
2022-03-12 19:29:17 -0500

    :sparkles: __name__ and __main__

---
+++ one.py
@@ -0,0 +1,9 @@
+
+def firstExecution():
+    print("Running inside one.py")
+
+
+if __name__ == "__main__":
+    print('One.py running directly')
+else:
+    print('ONE.py has been imported')
---
+++ two.py
@@ -0,0 +1,10 @@
+import one
+
+print("Top level in two.py")
+
+one.firstExecution()
+
+if __name__ == "main":
+    print("TWO.py is being run directly")
+else:
+    print("ONE.py has been called")
